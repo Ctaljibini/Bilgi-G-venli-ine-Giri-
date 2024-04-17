ac3a77905ed3d1de2bcca1dd11d29cba3d6695b1
Cuma TALJIBINI <taljibinicuma@gmail.com>
2024-04-17 18:39:32 +0300

    added vigenere class

---
+++ Homework-1/vigenereCipher/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static string alphaChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ().,?!";
+        Vigenere vigenere = new Vigenere();
+        public Form1()
+        {
+            InitializeComponent(); // sadece okumak için kullanıyorum.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            output.ReadOnly = true;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            string txt = inputBox.Text;
+            string key = keyBox.Text;
+            if (string.IsNullOrEmpty(txt) || string.IsNullOrEmpty(key))
+                MessageBox.Show("Please fill out the previous fields");
+            string cipher = vigenere.encryption(txt, key);
+            output.AppendText(cipher + Environment.NewLine);
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        // karakterlerin ineks numarası 0'dan 25'e
+      
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            string cipher = inputBox.Text;
+            string key = keyBox.Text;
+            
+            if (string.IsNullOrEmpty(cipher) || string.IsNullOrEmpty(key))
+                MessageBox.Show("Please fill out the previous fields");
+
+            string message = vigenere.decryption(cipher, key);
+            output.AppendText(message + Environment.NewLine);
+
+        }
+    }
+}
---
+++ Homework-1/vigenereCipher/Vigenere.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Vigenere
+    {
+        public static string alphaChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ().,?!";
+        public List<int> GetCharIndexes(string str)
+        {
+            List<int> charsAtPos = new List<int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                charsAtPos.Add(alphaChars.IndexOf(str[i]));
+            }
+            return charsAtPos;
+        }
+
+        public string encryption(string message, string key)
+        {
+            List<int> messageCharIndex = GetCharIndexes(message);
+            List<int> keyCharIndex = GetCharIndexes(key);
+            string cipher = "";
+
+            int textLength = message.Length;
+            int keyLength = key.Length;
+            int letter;
+
+            for (int n = 0; n < textLength; n++)
+            {
+                letter = (messageCharIndex[n] + keyCharIndex[n % keyLength]) % alphaChars.Length;
+                cipher += alphaChars[letter];
+            }
+            return cipher;
+        }
+
+        public string decryption(string cipher, string key)
+        {
+            List<int> cipherCharIndex = GetCharIndexes(cipher);
+            List<int> keyCharIndex = GetCharIndexes(key);
+
+            int cipherLength = cipher.Length;
+            int keyLength = key.Length;
+            string message = "";
+            int letter;
+
+            for (int n = 0; n < cipherLength; n++)
+            {
+                letter = (cipherCharIndex[n] - keyCharIndex[n % keyLength] + alphaChars.Length) % alphaChars.Length;
+                message += alphaChars[letter];
+            }
+            return message;
+        }
+    }
+}
